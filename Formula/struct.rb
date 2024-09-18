dc1ca45a902d40f2936d36815963c294f025e640
Ronald <ronald.cifuentes2020@gmail.com>
2024-09-18 08:21:14 -0500

    ✨ feat: Add mytool formula

---
+++ Formula/struct.rb
@@ -0,0 +1,19 @@
+# curl -L https://github.com/Ronald-Cifuentes/struct/archive/refs/tags/v1.0.0.tar.gz | shasum -a 256
+
+class Struct < Formula
+  desc "A CLI tool to generate folder structures like tree"
+  homepage "https://github.com/Ronald-Cifuentes/struct"
+  url "https://github.com/Ronald-Cifuentes/struct/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "759109173d59b726677286f72e56c941bdf3c078e84be268d54bbc2288e5f26f"
+  license "MIT"
+
+  def install
+    bin.install "bin/struct.sh" => "struct"
+    chmod 0755, bin/"struct" # Ensure the script is executable
+  end
+
+  test do
+    # Simple test to verify installation
+    assert_match "Usage", shell_output("#{bin}/struct --help")
+  end
+end
